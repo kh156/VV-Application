852ccd5d8e225133d1c4a3ebdc17372c2170ced8
Nick Gordon <nick.gordon@duke.edu>
2012-10-10 17:04:41 -0400

    commit

---
+++ VV-Application/InsulaViewController.h
@@ -0,0 +1,17 @@
+//
+//  InsulaViewController.h
+//  VV-Application
+//
+//  Created by Nicholas Gordon on 10/8/12.
+//  Copyright (c) 2012 Nicholas Gordon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface InsulaViewController : UIViewController
+
+@property(nonatomic, weak) IBOutlet UIScrollView *insulaScroller;
+@property(nonatomic, weak) IBOutlet UIView *insulaScrollerContent;
+
+
+@end
