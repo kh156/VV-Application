f31b49a5adfd0aa1ec9d81f139e82f08c8b6d9fc
Nick Gordon <nick.gordon@duke.edu>
2012-10-24 17:26:32 -0400

    restore repo

---
+++ VV-Application/InsulaViewController.h
@@ -0,0 +1,31 @@
+//
+//  InsulaViewController.h
+//  VV-Application
+//
+//  Created by Nicholas Gordon on 10/8/12.
+//  Copyright (c) 2012 Nicholas Gordon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <MapKit/Mapkit.h>
+#import "HomescreenViewcontroller.h"
+
+@interface InsulaViewController : UIViewController 
+
+@property(nonatomic, weak) IBOutlet UIScrollView *IVScroller;
+@property(nonatomic, weak) IBOutlet UIView *IVScrollerContent;
+@property(nonatomic, weak) IBOutlet UITextView *IVSummary;
+@property(nonatomic, weak) IBOutlet UIButton *IVButton;
+@property(nonatomic, weak) IBOutlet MKMapView *IVMapView;
+@property(nonatomic, weak) IBOutlet UISearchBar *IVSearchBar;
+@property(nonatomic, weak) IBOutlet UISlider *IVSlider;
+
+-(IBAction)landmark_button_touch:(UIButton *)sender;
+-(IBAction) slider_moved:(UISlider *)sender;
+-(void)setInitialMapRegion;
+-(void) plotMapAnnotation: (NSString *) name address:(NSString *) address latitude:(double) latitude longitude:(double) longitude;
+-(IBAction)landmarkSearch: (UIBarButtonItem *)sender;
+-(void) zoomOnAnnotation;
+
+
+@end
---
+++ VV-Application/LandmarkViewController.h
@@ -0,0 +1,24 @@
+//
+//  LandmarkViewController.h
+//  VV-Application
+//
+//  Created by Nicholas Gordon on 10/7/12.
+//  Copyright (c) 2012 Nicholas Gordon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <NinevehGL/NinevehGL.h>
+
+
+@interface LandmarkViewController : UIViewController <NGLViewDelegate> {
+    NGLMesh *mesh;
+    NGLCamera *camera;
+}
+
+@property float rotateX;
+@property float rotateY;
+@property float rotateZ;
+
+-(void) initNGL:(NSString *) fileName;
+
+@end
